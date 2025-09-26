4c158892eb3234e82b50934eb582b6cb749748d4
MichielN19 <Michielmitsjol@gmail.com>
2025-09-26 11:50:46 +0200

    Added my routes

--- app/Routes/routes.php
+++ app/Routes/routes.php
@@ -1,7 +1,27 @@
 <?php
 
 return [
+    // Get data
     'GET' => [
-        'thread' => [App\Http\Controllers\ThreadController::class, 'test'],
-    ]
+        '/' => [App\Http\Controllers\ThreadController::class, 'index'],
+        'threads' => [App\Http\Controllers\ThreadController::class, 'index'],
+        'thread' => [App\Http\Controllers\ThreadController::class, 'find'],
+        'topics' => [App\Http\Controllers\TopicController::class, 'index'],
+        'topic' => [App\Http\Controllers\TopicController::class, 'index'],
+        'replies' => [App\Http\Controllers\ReplyController::class, 'index'],
+        'reply' => [App\Http\Controllers\ReplyController::class, 'index'],
+    ],
+    // Create
+    'POST' => [
+        'thread' => [App\Http\Controllers\ThreadController::class, 'create'],
+        'topic' => [App\Http\Controllers\TopicController::class, 'create'],
+        'reply' => [App\Http\Controllers\ReplyController::class, 'create'],
+    ],
+    // Update
+    'PUT' => [
+        'thread' => [App\Http\Controllers\ThreadController::class, 'update'],
+        'topic' => [App\Http\Controllers\TopicController::class, 'update'],
+        'reply'  => [App\Http\Controllers\ReplyController::class, 'update'],
+    ],
+
 ];
