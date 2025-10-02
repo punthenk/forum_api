826ad40d26d8f0fde4e1ecec2e0a842ca4c0dcd0
MichielN19 <Michielmitsjol@gmail.com>
2025-10-02 14:42:46 +0200

    feat: finaly commited entry point

--- index.php
+++ index.php
@@ -1,15 +1,9 @@
 <?php
+
 @include('vendor/autoload.php');
 
-/* $routes = @include('app/Routes/routes.php'); */
+$routes = @include('app/Routes/routes.php');
 
-/* $requestHandler = new App\Http\RequestHandler($routes); */
+$requestHandler = new App\Http\RequestHandler($routes);
 
-use App\Models\ThreadModel;
-
-$data = $_POST ?? null;
-
-$data = ThreadModel::update($data);
-echo '<pre>';
-print_r($data);
-
+$requestHandler->handle();
